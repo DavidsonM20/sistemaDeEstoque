435099dbb6ab5b7fc87bc2cec4240f486704bd1b
DavidsonM20 <gabriel78miranda@gmail.com>
2026-02-20 19:33:01 -0300

    first commit

---
+++ src/main/java/login/Login.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package login;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author 232.969762
+ */
+public class Login extends HttpServlet{
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            thows ServletException, IOException {
+}
