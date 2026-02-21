b274753dd040907f40c2067f35aa96a9e4572935
DavidsonM20 <gabriel78miranda@gmail.com>
2026-02-20 21:31:05 -0300

    login

--- src/main/java/login/Login.java
+++ src/main/java/login/Login.java
@@ -4,15 +4,38 @@
  */
 package login;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  *
  * @author 232.969762
  */
+@WebServlet("/login")
 public class Login extends HttpServlet{
     
+    private static final long serialVersionUID = 1L;
+    
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            thows ServletException, IOException {
+            throws ServletException, IOException {
+        
+            String usuario = request.getParameter("users");
+            String senha = request.getParameter("passw");
+            
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            
+            if("Davidson".equals(usuario) && "1234".equals(senha)) {
+                response.sendRedirect("dashboard.html");
+                // out.println("<h2>Login realizado</h2>");
+            } else {
+                out.println("<h2>Usuario ou senha incorreta</h2>");
+            }
+             
+    }
 }
