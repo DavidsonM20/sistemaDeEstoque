8aeaa3351c2c5e03ccc308b1d12daa36dddb9243
DavidsonM20 <gabriel78miranda@gmail.com>
2026-02-26 19:09:00 -0300

    Configuração da tela de login com o banco de dados

--- src/main/java/login/Login.java
+++ src/main/java/login/Login.java
@@ -1,9 +1,10 @@
-/*
+
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package login;
 
+import connection.ConnectionFactory;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,30 +13,31 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
+
 /**
  *
  * @author 232.969762
  */
 @WebServlet("/login")
 public class Login extends HttpServlet{
-    
+   
     private static final long serialVersionUID = 1L;
-    
+   
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-            String usuario = request.getParameter("users");
-            String senha = request.getParameter("passw");
-            
+       
+            String usuario = request.getParameter("user");
+            String senha = request.getParameter("password");
+           
             response.setContentType("text/html");
-            PrintWriter out = response.getWriter();
+            PrintWriter out  = response.getWriter();
             
-            if("Davidson".equals(usuario) && "1234".equals(senha)) {
-                response.sendRedirect("dashboard.html");
-                // out.println("<h2>Login realizado</h2>");
-            } else {
-                out.println("<h2>Usuario ou senha incorreta</h2>");
-            }
-             
+            try (var con = ConnectionFactory.getConnection()) {
+            String sql = "SELECT * FROM users WHERE username = ? AND psw = ?";
+        } catch (Exception e) {
+        }
+            
     }
+    
 }
